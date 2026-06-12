fb84482d32adc7167dd26349aebcad000db77bf4
unknown <Lanvender4444@gmail.com>
2026-06-12 15:59:50 +0800

    add in one file

--- run.ps1
+++ run.ps1
@@ -1,4 +1,6 @@
 $env:PATH = "C:\winlibs\mingw64\bin;" + $env:PATH
-$env:GOPROXY = "https://goproxy.cn,direct"
+
+# 如果在国内则代理
+# $env:GOPROXY = "https://goproxy.cn,direct"
 $env:CGO_ENABLED = "1"
 go run .
